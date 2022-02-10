0738eef67d12015d6aabe8e3018609c79a3bc4b6
egorova-valeria <89708287+egorova-valeria@users.noreply.github.com>
2022-02-10 12:08:56 +0400

    v1

---
+++ homework_4.js
@@ -0,0 +1,40 @@
+﻿// Задание 4
+// Напишите функцию, которая принимает два числа. 
+// Каждую секунду необходимо выводить в консоль, 
+// начиная от первого и заканчивая вторым. Используйте setInterval.
+
+// Например, пользователь ввёл числа 5 и 15. 
+// Каждую секунду в консоль должно печататься число, 
+// начиная с 5 и заканчивая 15 (всего 11 чисел: 5 6 7 8 9 10 11 12 13 14 15).
+
+function printNumbers(from, to) {
+  let current = from;
+  
+  let timerId = setInterval(function() {
+    console.log(current);
+  if (current == to) {
+    clearInterval(timerId);
+  } 
+    current++;
+    }, 1000);
+}
+
+printNumbers(2, 10)
+
+
+
+// вариант 2, через setTimeout
+
+// function printNumbers(from, to) {
+//   let current = from;
+
+//   setTimeout(function print() {
+//     console.log(current);
+//     if (current < to) {
+//       setTimeout(print, 1000);
+//     }
+//     current++;
+//   }, 1000);
+// }
+
+// printNumbers(2, 5)
